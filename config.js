fcb71fec16df670cc7cbf0f879295fea2985b1d3
uiwwnw Yoon <uiwwnwyoon@uiwwnwui-MacBook.local>
2019-02-04 15:05:59 +0900

    d

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    dburl: "mongodb+srv://admin:xoqhi1-nijziQ-nojfeb@cluster0-dbn5k.gcp.mongodb.net/",
-    key: 'paprika1213',
-    daumapi: "d800cd9a3ef7f463dc8064fd37d15106"
-};
